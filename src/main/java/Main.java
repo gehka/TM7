8204d19e4eb21552d2954389025eabb7eff7c7d1
gekrin <Ns8TZyUbNR8JKFiWyP0A>
2020-06-26 15:04:23 +0200

    aufgabe 1b+c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
+import org.apache.commons.math3.random.JDKRandomGenerator;
+import org.apache.commons.math3.random.RandomDataGenerator;
+
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        Random rand1 = new Random(1000);
-        Random rand2 = new Random(1000);
+        JDKRandomGenerator myGen1 = new JDKRandomGenerator();
+        myGen1.setSeed(100);
+        RandomDataGenerator rdg1 = new RandomDataGenerator(myGen1);
+
+        JDKRandomGenerator myGen2 = new JDKRandomGenerator();
+        myGen2.setSeed(100);
+        RandomDataGenerator rdg2 = new RandomDataGenerator(myGen2);
 
         for (int i = 0; i < 15; i++) {
-            System.out.println(rand1.nextInt());
-            System.out.println(rand2.nextInt());
+            System.out.println(rdg1.nextInt(0,100));
+            System.out.println(rdg2.nextInt(0,100));
         }
     }
 }
